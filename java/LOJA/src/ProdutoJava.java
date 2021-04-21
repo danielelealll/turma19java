ac2a2f01bb816f29fab937c6bd826a33c62ec7e0
Daniele Leal <80607069+danielelealll@users.noreply.github.com>
2021-04-21 15:54:25 -0300

    Update ProdutoJava.java

--- java/LOJA/src/ProdutoJava.java
+++ java/LOJA/src/ProdutoJava.java
@@ -1,4 +1,25 @@
+package application;
 
-public class ProdutoJava {
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Produto;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
+
+		Produto product1 = new Produto("XL1", "Camisa", 100.55);
+
+		product1.incluirEstoque(10);
+
+		product1.setValorUnitario(80.99);
+		System.out.println(product1.getDescricao() + " R$" + product1.getValorUnitario() + " - Qtde em estoque: "
+				+ product1.getQtdeEstoque());
+
+	}
 
 }
