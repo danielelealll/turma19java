5189da339f63227021898abbd09183852feb3afe
Daniele Leal <80607069+danielelealll@users.noreply.github.com>
2021-04-21 15:45:40 -0300

    Update Estudante.java

--- java/ESCOLA/src/entities/Estudante.java
+++ java/ESCOLA/src/entities/Estudante.java
@@ -1,5 +1,48 @@
 package entities;
 
 public class Estudante {
+	
+	private int matricula;
+	private String cpf;
+	private double pontos;
+	private boolean status;
+	
+	public Estudante(int matricula, String cpf) {
+		super();
+		this.matricula = matricula;
+		this.cpf = cpf;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	/*public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}*/
+	public String getCpf() {
+		return cpf;
+	}
+	/*public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}*/
+	public double getPontos() {
+		return pontos;
+	}
+	/*public void setPontos(double pontos) {
+		this.pontos = pontos;
+	}*/
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	public void adicionarNota(double valorPonto) {
+		this.pontos += valorPonto;
+	}
+	
+	public void tirarNota(double valorPonto) {
+		this.pontos -= valorPonto;
+	}
 
 }
