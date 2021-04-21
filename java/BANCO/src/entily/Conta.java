b0f82afaaf2b6a0f86ba9ef485424869edd5365f
Daniele Leal <80607069+danielelealll@users.noreply.github.com>
2021-04-21 15:43:37 -0300

    Update Conta.java

--- java/BANCO/src/entily/Conta.java
+++ java/BANCO/src/entily/Conta.java
@@ -1,5 +1,32 @@
-package entily;
+package entities;
 
 public class Conta {
-
+	
+	public int numero;
+	public String cpf;
+	private double saldo;
+	public boolean ativa;
+	
+	public Conta(int numero, String cpf) {
+		this.numero = numero;
+		this.cpf = cpf;
+	}
+	
+	public String getCpf() {
+		return this.cpf;
+	}
+	
+	public Conta(int numero, String cpf, boolean ativa) {
+		this.numero = numero;
+		this.cpf = cpf;
+		this.ativa = ativa;
+	}
+	
+	public void debito(double valor) {
+		saldo -= valor;
+	}
+	
+	public void credito(double valor) {
+		saldo += valor;
+	}
 }
