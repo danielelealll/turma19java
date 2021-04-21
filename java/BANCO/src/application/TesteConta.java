e70df84020bf6be2df3459a02f2fec0633e03d74
Daniele Leal <80607069+danielelealll@users.noreply.github.com>
2021-04-21 15:42:44 -0300

    Update TesteConta.java

--- java/BANCO/src/application/TesteConta.java
+++ java/BANCO/src/application/TesteConta.java
@@ -1,9 +1,40 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Conta;
+
 public class TesteConta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
+		
+		Conta contaPoupança = new Conta(1001, "401.676.758-00");
+		Conta contaCorrente = new Conta(1020, "401.676.758-00");
+		
+		System.out.println("Digite o numero da conta: ");
+		contaPoupança.numero = leia.nextInt();
+		System.out.println("Digite o CPF: ");
+		contaPoupança.cpf = leia.next();
+		System.out.println("Operação de 1-Credito ou 2-Debito: ");
+		char opcao = leia.next().charAt(0);
+		System.out.println("Qual valor: ");
+		double valor = leia.nextDouble();
+		
+		if(opcao == '1') {
+			contaPoupança.credito(valor);
+		} else if(opcao == '2') {
+			contaPoupança.debito(valor);
+		}
+		
+		System.out.printf("CPF do cliente: %s", contaPoupança.cpf);
+		System.out.printf("\nNumero da conta: %d", contaPoupança.numero);
+		System.out.printf("\nCPF do cliente: %.2f", contaPoupança.saldo);
+		
+		
 
 	}
 
